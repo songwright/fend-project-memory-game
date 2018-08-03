67b6c737d10855d574aeef6a860767c73466d2a2
Ramon Sanchez <songwright66@gmail.com>
2018-08-03 12:05:53 -0700

    feat: Create a list that holds all the cards in the deck

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * Create a list that holds all of your cards
  */
 
+ const cardList = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-bomb'];
 
 /*
  * Display the cards on the page
