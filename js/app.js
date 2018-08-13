d288eb7aefb7e69d8d3776219b3d17b7721022f3
Ramon Sanchez <songwright66@gmail.com>
2018-08-13 11:25:41 -0700

    style: Clean up white space formatting

--- js/app.js
+++ js/app.js
@@ -52,24 +52,23 @@ assignCards();
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
- // Click on a card to open it
- document.addEventListener('click', function(event){
-   const clickedOnCard = event.target;
-   if (clickedOnCard.className === 'card') { // Restrict the event listener to only act on cards
-     showCard(event);
-     addCard(event);
-    }
-   }
- );
+// Click on a card to open it
+document.addEventListener('click', function(event){
+  const clickedOnCard = event.target;
+  if (clickedOnCard.className === 'card') { // Restrict the event listener to only act on cards
+    showCard(event);
+    addCard(event);
+  }
+});
 
 // Show the card
 function showCard(evt){
-   var clickedOnCell = evt.target;
-   clickedOnCell.className = 'card open';
- }
+  var clickedOnCell = evt.target;
+  clickedOnCell.className = 'card open';
+}
 
- // Add the card to the list of open cards.
- function addCard(evt){
-   const clickedOnCard = evt.target;
-   openCardList.push(clickedOnCard.firstElementChild.id); // Put the id of the card in the list of open cards.
- }
+// Add the card to the list of open cards.
+function addCard(evt){
+  const clickedOnCard = evt.target;
+  openCardList.push(clickedOnCard.firstElementChild.id); // Put the id of the card in the list of open cards.
+}
