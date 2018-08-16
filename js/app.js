aeb82cdb2bcec05ec6455000c686eabd6e128a91
Ramon Sanchez <songwright66@gmail.com>
2018-08-16 09:04:13 -0700

    feat: Add random symbol change effect to fail result

--- js/app.js
+++ js/app.js
@@ -118,6 +118,12 @@ function moveSucceed () {
 function moveFail () {
     const mismatch1 = document.getElementById(openCardList[0]);
     const mismatch2 = document.getElementById(openCardList[1]);
+    const symbolList = ["fa fa-frown-o", "fa fa-ban", "fa fa-thumbs-o-down"];
+    randomIndex = Math.floor(Math.random() * 3);
+    const failureSymbol = symbolList[randomIndex];
+    // Change the symbols to failure symbols.
+    mismatch1.className = failureSymbol;
+    mismatch2.className = failureSymbol;
     mismatch1.parentElement.className = 'card fail';
     mismatch2.parentElement.className = 'card fail';
     setTimeout(faceCardsDown, 1000); // Wait for card fail and card transition to finish
@@ -127,6 +133,12 @@ function moveFail () {
 function faceCardsDown () {
     const mismatch1 = document.getElementById(openCardList[0]); // Get the first card in the card list.
     const mismatch2 = document.getElementById(openCardList[1]); // Get the second card in the card list.
+    const cardListIndex1 = openCardList[0].slice(4); // Get the index number of the first card's id.
+    const cardListIndex2 = openCardList[1].slice(4); // Get the index number of the second card's id.
+    // Return the symbols to their original values from cardList.
+    mismatch1.className = cardList[cardListIndex1];
+    mismatch2.className = cardList[cardListIndex2];
+    // Turn the cards face down.
     mismatch1.parentElement.className = 'card';
     mismatch2.parentElement.className = 'card';
     openCardList.splice(0,2); // Reset the open card list.
